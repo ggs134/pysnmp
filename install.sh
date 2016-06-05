ed34489ce78f9ee36b7630505c72007c13dbe622
ggs134 <inditow@gmail.com>
2016-06-06 08:56:45 +0900

    screen install added

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!bin/bash
 
-sudo apt-get -y update                               #업데이트
-sudo apt-get -y install python-pip 	         #pip 설치
-sudo pip install -y --upgrade pip                 #pip업그레이드
-sudo apt-get -y install python-dev              #파이썬 디벨로퍼 버전 설치
-sudo pip install pysnmp                         #pysnmp설치
-sudo apt-get install -y libsmi2ldbl
-sudo apt-get install -y python-pysnmp4
-sudo apt-get install -y smilint
-sudo apt-get install -y snmp snmp-mibs-downloader   #MIB데이터 다운로드
-sudo apt-get install -y supervisor
+apt-get -y update                               #업데이트
+apt-get -y install python-pip 	         #pip 설치
+pip install -y --upgrade pip                 #pip업그레이드
+apt-get -y install python-dev              #파이썬 디벨로퍼 버전 설치
+pip install pysnmp                         #pysnmp설치
+apt-get install -y libsmi2ldbl
+apt-get install -y python-pysnmp4
+apt-get install -y smilint
+apt-get install -y snmp snmp-mibs-downloader   #MIB데이터 다운로드
+apt-get install -y supervisor screen
 
 ##supervisor세팅
-mkdir -p '$home/log'
-#monitor.conf파일 설치
+mkdir -p '$HOME/log'
 
+#monitor.conf파일 세팅
 #agent.conf파일 세팅
+python "monitor_and_agent_install.py"
+
+#supervisor에서 읽어들임
+supervisorctl reread
+supervisorctl update
