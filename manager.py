39f3d7613c24cd7e37005a9821e229d58e3ba419
ggs134 <inditow@gmail.com>
2016-06-06 03:01:10 +0900

    agetn.conf file added

--- manager.py
+++ manager.py
@@ -6,11 +6,13 @@ cmdGen = cmdgen.CommandGenerator()
 errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
     cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
     cmdgen.UdpTransportTarget(('onther.iptime.org', 161)),
-    cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./')
+    cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
+    cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./')
 )
 
 # Check for errors and print out results
 if errorIndication:
+    print "hello"
     print(errorIndication)
 else:
     if errorStatus:
@@ -22,5 +24,7 @@ else:
     else:
         for name, val in varBinds:
             print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
-	    print(errorStatus)
-	    print(errorIndication)
+        #0 if no problem
+        print(errorStatus)
+        #None if no problem
+        print(errorIndication)
