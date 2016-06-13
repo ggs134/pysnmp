f73af2432d48a5cd0985a206cf103333fc423b32
ggs134 <inditow@gmail.com>
2016-06-14 07:16:51 +0900

    install3.sh changed

--- install3.sh
+++ install3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+user=`echo "$HOME" | cut -d "/" -f3`
+
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get -y update
 sudo apt-get install git cmake libcryptopp-dev libleveldb-dev libjsoncpp-dev libjsonrpccpp-dev libboost-all-dev libgmp-dev libreadline-dev libcurl4-gnutls-dev ocl-icd-libopencl1 opencl-headers mesa-common-dev libmicrohttpd-dev build-essential -y
@@ -14,15 +16,15 @@ sudo make install
 ethminer -G --list-devices
 sudo service supervisor start
 sudo rm /etc/supervisor/conf.d/ethminer.conf
-echo "[program:ethminer]" >> /etc/supervisor/conf.d/ethminer.conf
-echo "command=ethminer --farm-recheck 2000 -G -S asia1.ethpool.org:3333 -O 0xa47cd1e0e031de09622b6ada5f80a291f302e711.$USER --cl-global-work 16384 --cl-local-work 256 -E old" >> /etc/supervisor/conf.d/ethminer.conf
-echo "directory=$HOME" >> /etc/supervisor/conf.d/ethminer.conf
-echo "autostart=true" >> /etc/supervisor/conf.d/ethminer.conf
-echo "autorestart=true" >> /etc/supervisor/conf.d/ethminer.conf
-echo "startretries=3" >> /etc/supervisor/conf.d/ethminer.conf
-echo "stdout_logfile=$HOME/ethminer.out.log" >>/etc/supervisor/conf.d/ethminer.conf
-echo "stderr_logfile=$HOME/ethminer.err.log">>/etc/supervisor/conf.d/ethminer.conf
-echo "user=$USER" >> /etc/supervisor/conf.d/ethminer.conf
+sudo echo "[program:ethminer]" >> /etc/supervisor/conf.d/ethminer.conf
+sudo echo "command=ethminer --farm-recheck 2000 -G -S asia1.ethpool.org:3333 -O 0xa47cd1e0e031de09622b6ada5f80a291f302e711.$user --cl-global-work 16384 --cl-local-work 256 -E old" >> /etc/supervisor/conf.d/ethminer.conf
+sudo echo "directory=$HOME" >> /etc/supervisor/conf.d/ethminer.conf
+sudo echo "autostart=true" >> /etc/supervisor/conf.d/ethminer.conf
+sudo echo "autorestart=true" >> /etc/supervisor/conf.d/ethminer.conf
+sudo echo "startretries=3" >> /etc/supervisor/conf.d/ethminer.conf
+sudo echo "stdout_logfile=$HOME/ethminer.out.log" >>/etc/supervisor/conf.d/ethminer.conf
+sudo echo "stderr_logfile=$HOME/ethminer.err.log">>/etc/supervisor/conf.d/ethminer.conf
+sudo echo "user=$user" >> /etc/supervisor/conf.d/ethminer.conf
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo reboot
