fe69dd0b36abccbb2a2bbdf83a4adda51d3f40fc
ggs134 <inditow@gmail.com>
2016-07-26 00:02:59 -0700

    agent updated

--- agent.py
+++ agent.py
@@ -3,14 +3,14 @@ from pysnmp.entity.rfc3413 import cmdrsp, context
 from pysnmp.carrier.asynsock.dgram import udp
 from pysnmp.smi import builder
 
+from getdata import DataHouse
+
 import threading
 import collections
 import time
 import sys
 import os
 import json
-
-import readGPUTemp
 
 filepath = os.path.dirname(os.path.realpath(__file__))
 
@@ -26,40 +26,144 @@ with open(filepath+"/settings.txt") as settings:
 
 class CustomMib(object):
 #=====================================================================================
-    def __init__(self, path):
+    def __init__(self):
         self._lock = threading.RLock()
-        self._hashrate = 0
-        self._file_path = path
+        self._house = DataHouse
+        self._virtual = self._house.virtualMemory
+        self._network = self._house.networkUsage
+        self._ethminer = self._house.ethminerProcess.as_dict()
     #=====================================================================================
     def getDescription(self):
-        res = readGPUTemp.parseResult()
-        return res
+        return "This is test description"
     #=====================================================================================
     def getHashrate(self):
         with self._lock:
     # self._hashrate = self._getHash()
-            return self._getHash()
+            return 0
+    def getGpuTemprature(self):
+        with self._lock:
+            return self._house.temperature
+    def getGpuLoad(self):
+        with self._lock:
+            return self._house.gpuLoad
+    def getCoreClock(self):
+        with self._lock:
+            return self._house.coreClock
+    def getMemoryClock(self):
+        with self._lock:
+            return self._house.memoryClock
+    def getCpuPercent(self):
+        with self._lock:
+            return self._house.cpuPercent
+    def getVirtualTotal(self):
+        with self._lock:
+            return self._virtual.total
+    def getVirtualAvailable(self):
+        with self._lock:
+            return self._virtual.available
+    def getVirtualPercent(self):
+        with self._lock:
+            return self._virtual.percent
+    def getVirtualUsed(self):
+        with self._lock:
+            return self._virtual.used
+    def getVirtualFree(self):
+        with self._lock:
+            return self._virtual.free
+    def getVirtualActive(self):
+        with self._lock:
+            return self._virtual.active
+    def getVirtualInactive(self):
+        with self._lock:
+            return self._virtual.inactive
+    def getBytesSent(self):
+        with self._lock:
+            return self._network.bytes_sent
+    def getBytesRecv(self):
+        with self._lock:
+            return self._network.bytes_recv
+    def getPacketsSent(self):
+        with self._lock:
+            return self._network.packets_sent
+    def getPacketsRecv(self):
+        with self._lock:
+            return self._network.packets_recv
+    def getErrin(self):
+        with self._lock:
+            return self._network.errin
+    def getErrout(self):
+        with self._lock:
+            return self._network.errout
+    def getDropIn(self):
+        with self._lock:
+            return self._network.dropin
+    def getDropOut(self):
+        with self._lock:
+            return self._network.dropout
+    def getBootTime(self):
+        with self._lock:
+            return self._house.bootTime
+    def getEthminerStatus(self):
+        with self._lock:
+            return self._ethminer["status"]
+    def getEthminerCPUPercent(self):
+        with self._lock:
+            return self._ethminer["cpu_percent"]
+    def getEthminerMemoryPercent(self):
+        with self._lock:
+            return self._ethminer["memory_percent"]
+    def getEthminerRss(self):
+        with self._lock:
+            return self._ethminer["memory_info"].rss
+    def getEthminerVms(self):
+        with self._lock:
+            return self._ethminer["memory_info"].vms
+    def getEthminerShared(self):
+        with self._lock:
+            return self._ethminer["memory_info"].shared
+    def getEthminerText(self):
+        with self._lock:
+            return self._ethminer["memory_info"].text
+    def getEthminerLib(self):
+        with self._lock:
+            return self._ethminer["memory_info"].lib
+    def getEthminerData(self):
+        with self._lock:
+            return self._ethminer["memory_info"].data
+    def getEthminerDirty(self):
+        with self._lock:
+            return self._ethminer["memory_info"].dirty
+    def getEthminerUss(self):
+        with self._lock:
+            return self._ethminer["memory_full_info"].uss
+    def getEthminerPss(self):
+        with self._lock:
+            return self._ethminer["memory_full_info"].pss
+    def getEthminerSwap(self):
+        with self._lock:
+            return self._ethminer["memory_full_info"].swap
+    def getEthminerReadCount(self):
+        with self._lock:
+            return self._ethminer["io_counters"].read_count
+    def getEthminerWriteCount(self):
+        with self._lock:
+            return self._ethminer["io_counters"].write_count
+    def getEthminerReadBytes(self):
+        with self._lock:
+            return self._ethminer["io_counters"].read_bytes
+    def getEthminerWriteBytes(self):
+        with self._lock:
+            return self._ethminer["io_counters"].write_bytes
+    def getEthminerNumThreads(self):
+        with self._lock:
+            return self._ethminer["num_threads"]
+    def getEthminerCTXVolSwitches(self):
+        with self._lock:
+            return self._ethminer['num_ctx_switches'].voluntary
+    def getEthminerCTXInvloSwitches(self):
+        with self._lock:
+            return self._ethminer['num_ctx_switches'].involuntary
 
-    def _getHash(self):
-        with open(self._file_path) as f:
-            lines = f.readlines()
-            last = lines[-1]
-            split = last.split()
-
-            # if len(split)<7:
-            #     res = 10
-            #
-            # elif split[2].startswith("Mining"):
-            #     res = 10
-            #
-            # else:
-            #     res = int(split[7])
-            try:
-                res = float(split[8][:-4])
-            except Exception as e:
-                res = 0
-    # print res
-            return res
 
 	# #=====================================================================================
 	# def setTestCount(self, value):
@@ -90,9 +194,50 @@ def createVariable(SuperClass, getValue, *args):
 
 #amin code
 
-cmib = CustomMib(str(sys.argv[1]))
-objects = [MibObject('MY-MIB', 'hashrateDescription', cmib.getDescription), MibObject('MY-MIB', 'hashrate', cmib.getHashrate)]
-
+cmib = CustomMib()
+objects =[]
+objects.append(MibObject('MY-MIB', 'hashrateDescription', cmib.getDescription))
+objects.append(MibObject('MY-MIB', 'hashrate', cmib.getHashrate))
+objects.append(MibObject('MY-MIB', 'gpuTemperature', cmib.getGpuTemprature))
+objects.append(MibObject('MY-MIB', 'gpuLoad', cmib.getGpuLoad))
+objects.append(MibObject('MY-MIB', 'coreClock', cmib.getCoreClock))
+objects.append(MibObject('MY-MIB', 'cpuPercent', cmib.getCpuPercent))
+objects.append(MibObject('MY-MIB', 'virtualTotal', cmib.getVirtualTotal))
+objects.append(MibObject('MY-MIB', 'virtualAvailable', cmib.getVirtualAvailable))
+objects.append(MibObject('MY-MIB', 'virtualPercent', cmib.getVirtualPercent))
+objects.append(MibObject('MY-MIB', 'virtualUsed', cmib.getVirtualUsed))
+objects.append(MibObject('MY-MIB', 'virtualFree', cmib.getVirtualFree))
+objects.append(MibObject('MY-MIB', 'virtualActive', cmib.getVirtualActive))
+objects.append(MibObject('MY-MIB', 'virtualInactive', cmib.getVirtualInactive))
+objects.append(MibObject('MY-MIB', 'bytesSent', cmib.getBytesSent))
+objects.append(MibObject('MY-MIB', 'bytesRecv', cmib.getBytesRecv))
+objects.append(MibObject('MY-MIB', 'packetsSent', cmib.getPacketsSent))
+objects.append(MibObject('MY-MIB', 'packetsRecv', cmib.getPacketsRecv))
+objects.append(MibObject('MY-MIB', 'errin', cmib.getErrin))
+objects.append(MibObject('MY-MIB', 'errout', cmib.getErrout))
+objects.append(MibObject('MY-MIB', 'dropIn', cmib.getDropIn))
+objects.append(MibObject('MY-MIB', 'dropOut', cmib.getDropOut))
+objects.append(MibObject('MY-MIB', 'bootTime', cmib.getBootTime))
+objects.append(MibObject('MY-MIB', 'ethminerStatus', cmib.getEthminerStatus))
+objects.append(MibObject('MY-MIB', 'ethminerCPUPercent', cmib.getCPUPercent))
+objects.append(MibObject('MY-MIB', 'ethminerMemoryPercent', cmib.getEthminerMemoryPercent))
+objects.append(MibObject('MY-MIB', 'ethminerRss', cmib.getEthminerRss))
+objects.append(MibObject('MY-MIB', 'ethminerVms', cmib.getEthminerVms))
+objects.append(MibObject('MY-MIB', 'ethminerShared', cmib.getEthminerShared))
+objects.append(MibObject('MY-MIB', 'ethminerText', cmib.getEthminerText))
+objects.append(MibObject('MY-MIB', 'ethminerLib', cmib.getEthminerLib))
+objects.append(MibObject('MY-MIB', 'ethminerData', cmib.getEthminerData))
+objects.append(MibObject('MY-MIB', 'ethminerDirty', cmib.getEthminerDirty))
+objects.append(MibObject('MY-MIB', 'ethminerUss', cmib.getEthminerUss))
+objects.append(MibObject('MY-MIB', 'ethminerPss', cmib.getEthminerPss))
+objects.append(MibObject('MY-MIB', 'ethminerSwap', cmib.getEthminerSwap))
+objects.append(MibObject('MY-MIB', 'ethminerReadCount', cmib.getEthminerReadCount))
+objects.append(MibObject('MY-MIB', 'ethminerWriteCount', cmib.getEthminerWriteCount))
+objects.append(MibObject('MY-MIB', 'ethminerReadBytes', cmib.getEthminerReadBytes))
+objects.append(MibObject('MY-MIB', 'ethminerWriteBytes', cmib.getEthminerWriteBytes))
+objects.append(MibObject('MY-MIB', 'ethminerNumThreads', cmib.getEthminerNumThreads))
+objects.append(MibObject('MY-MIB', 'ethminerCTXVolSwitches', cmib.getEthminerCTXVolSwitches))
+objects.append(MibObject('MY-MIB', 'ethminerCTXInvolSwitches', cmib.getEthminerCTXInvloSwitches))
 
 
 snmpEngine = engine.SnmpEngine()
