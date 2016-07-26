3ace845a069d895862b971ececb9b0ae8884d922
ggs134 <inditow@gmail.com>
2016-07-26 01:22:35 -0700

    async manager updated

--- asyncManager.py
+++ asyncManager.py
@@ -5,40 +5,40 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 targets = (
     # 1-st target (SNMPv3 over IPv4/UDP)
     ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
-      cmdgen.UdpTransportTarget(('onther.iptime.org', 60001)),
+      cmdgen.UdpTransportTarget(('192.168.0.39', 161)),
       ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
         cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
     # 2-st target (SNMPv3 over IPv4/UDP)
     ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
-      cmdgen.UdpTransportTarget(('onther.iptime.org', 60003)),
+      cmdgen.UdpTransportTarget(('192.168.0.36', 161)),
       ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
         cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
-    # 3-st target (SNMPv3 over IPv4/UDP)
-    ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
-      cmdgen.UdpTransportTarget(('onther.iptime.org', 60004)),
-      ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
-        cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
-    # 4-st target (SNMPv1 over IPv4/UDP)
-    ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
-      cmdgen.UdpTransportTarget(('onther.iptime.org', 60005)),
-      ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
-        cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
-
-    # 5-st target (SNMPv1 over IPv4/UDP)
-    ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
-      cmdgen.UdpTransportTarget(('onther.iptime.org', 60006)),
-      ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
-        cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
-    # 6-st target (SNMPv1 over IPv4/UDP)
-    ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
-      cmdgen.UdpTransportTarget(('onther.iptime.org', 60007)),
-      ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
-        cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
-    # 7-st target (SNMPv1 over IPv4/UDP)
-    ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
-      cmdgen.UdpTransportTarget(('onther.iptime.org', 60008)),
-      ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
-        cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
+    # # 3-st target (SNMPv3 over IPv4/UDP)
+    # ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
+    #   cmdgen.UdpTransportTarget(('onther.iptime.org', 60004)),
+    #   ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
+    #     cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
+    # # 4-st target (SNMPv1 over IPv4/UDP)
+    # ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
+    #   cmdgen.UdpTransportTarget(('onther.iptime.org', 60005)),
+    #   ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
+    #     cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
+    #
+    # # 5-st target (SNMPv1 over IPv4/UDP)
+    # ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
+    #   cmdgen.UdpTransportTarget(('onther.iptime.org', 60006)),
+    #   ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
+    #     cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
+    # # 6-st target (SNMPv1 over IPv4/UDP)
+    # ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
+    #   cmdgen.UdpTransportTarget(('onther.iptime.org', 60007)),
+    #   ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
+    #     cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
+    # # 7-st target (SNMPv1 over IPv4/UDP)
+    # ( cmdgen.UsmUserData('goldrush', authKey="authkey1", privKey="privkey1", authProtocol=cmdgen.usmHMACMD5AuthProtocol, privProtocol=cmdgen.usmDESPrivProtocol),
+    #   cmdgen.UdpTransportTarget(('onther.iptime.org', 60008)),
+    #   ( cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,2,0).addAsn1MibSource('./'),
+    #     cmdgen.MibVariable('SNMPv2-SMI', 'enterprises',42,1,0).addAsn1MibSource('./') ) ),
 )
 
 # Wait for responses or errors
